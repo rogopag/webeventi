73512644c0c28485e7112895ed08d9ca17075cf0
Riccardo Strobbia <riccardo@robanostra.com>
2012-02-13 16:38:42 +0100

    First commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+<!-- begin footer -->
+<?php wp_footer(); ?>
+</div>
+</div>
+<p id="footer">&copy; <?php echo date("Y"); ?> <?php echo get_bloginfo('name'); ?>, powered by <a href="http://wordpress.org/">WordPress</a>, theme <a href="http://www.ich-bin-ein-geek-holt-mich-hier-raus.de/?p=774">SidebarsSuck</a> by <a href="http://lifecheater.ich-geek.de/">Lifecheater</a>, background by <a href="http://www.squidfingers.com/patterns/">Squidfingers</a></p>
+</body>
+</html>
