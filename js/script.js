40b57442790576160c9c1f9413db9488c167c4ef
Riccardo Strobbia <riccardo@robanostra.com>
2012-02-15 17:36:34 +0100

    added site url to js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var jq = jQuery;
+var jq = jQuery, Webeventi;
 
 jq(document).ready(function()
 {
@@ -6,7 +6,11 @@ jq(document).ready(function()
 		scrollAndDisplayLightbox();
 		doScroll();
 	}
-	jq('li.page_item a').corner("6px round");	
+	jq('li.page_item a').corner("6px round");
+	
+	jq('div.header_contenitore_left').click(function(){
+		window.location = Webeventi.site_url;
+	});	
 });
 
 var scrollAndDisplayLightbox = function(){
