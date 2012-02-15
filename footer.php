e6cea9c5fe4cfafa12840b1209bae874a50d4790
Riccardo Strobbia <riccardo@robanostra.com>
2012-02-15 15:16:23 +0100

    Changed markup struct

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <!-- begin footer -->
 <?php wp_footer(); ?>
-</div><!-- WRAP -->
-</div><!-- CONTAINER -->
+
 </div><!-- PAGE -->
 <div id="footer">&copy; <?php echo date("Y"); ?> <?php echo get_bloginfo('name'); ?>, powered by <a href="http://wordpress.org/">WordPress</a></div>
 </body>
